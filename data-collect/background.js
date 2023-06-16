a860c87b1a5ace8dad926391ffadcdf6946d6d88
xueg <xueg@wfanxin@163.com>
2023-06-16 23:22:47 +0800

    优化webRequest

--- data-collect/background.js
+++ data-collect/background.js
@@ -1,9 +1,30 @@
 console.log('加载background.js')
 
+const request_url = []
 chrome.webRequest.onBeforeRequest.addListener(function(details){
-	// if (details.url.indexOf('citymenu') != -1) {
-		console.log(details)
-	// }
-    
-
+	if (details.type === 'xmlhttprequest') {
+		let url = details.url.split('?')[0]
+		if (url.indexOf('api/') !== -1) {
+			request_url[url.substr(url.indexOf('api/'))] = details.url
+		}
+		
+	}
 }, {urls:['<all_urls>']}, ['blocking'])
+
+chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
+	let data = []
+	const url = request_url[req.data['url']]
+	if (url !== undefined) {
+		$.ajax({
+	        url: url,
+	        method: 'get',
+	        dataType: 'json',
+	        async: false,
+	        success: function(res) {
+	            data = res
+	        }
+	    })
+	}
+    
+    sendResponse(data)
+})
--- data-collect/content.js
+++ data-collect/content.js
@@ -1,1 +1,19 @@
 console.log('加载content.js')
+
+window.onload = async () => {
+	// 页面获取
+	var link_arr = []
+	$(".js_site-item").each(function(index, data){
+		link_arr.push(data.innerText)
+	})
+	console.log('页面获取结果:',link_arr)
+
+	// 接口获取
+	chrome.runtime.sendMessage({
+		data: {
+			'url': 'api/citymenu'
+		}
+	}, res => {
+		console.log('citymenu接口获取结果:',res)
+	})
+}
